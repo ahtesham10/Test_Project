44668a67a2dc810ea8ad6bd4488b21da8a8a973a
DELL <DELL@DESKTOP-U7TT4C1>
2021-08-11 21:10:49 +0530

    Addition method created by sharvari

---
+++ src/GitHub_Package/Code_for_addition.java
@@ -0,0 +1,20 @@
+package GitHub_Package;
+
+public class Code_for_addition {
+
+	public static void main(String[] args) {
+		
+		Addition();
+
+	}
+	
+	public static void Addition()
+	{
+		int a =10;
+		int b=5;
+		int c=a+b;
+		System.out.println("Addition is "+c);
+		
+	}
+
+}
