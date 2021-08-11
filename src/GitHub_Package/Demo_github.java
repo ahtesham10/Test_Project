d3ef99262107f9d71a64d5e3807c69013394351e
Ahtesham <Ahtesham@DESKTOP-RH8706V>
2021-08-11 19:57:52 +0530

    Changes made by Ahtesham in 1st Push

--- src/GitHub_Package/Demo_github.java
+++ src/GitHub_Package/Demo_github.java
@@ -4,7 +4,7 @@ public class Demo_github {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hii..GitHub..!!");
+		System.out.println("Hii..I'm 1st Demo GitHub..!!");
 
 	}
 
---
+++ src/GitHub_Package/Sharvari.java
@@ -0,0 +1,11 @@
+package GitHub_Package;
+
+public class Sharvari {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hii..Sharvari & Dipak");
+
+	}
+
+}
